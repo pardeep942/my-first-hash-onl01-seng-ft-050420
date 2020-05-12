8feed1e1687dd1a9fd3b933d363fb81226306e17
PARDEEP KAUR <pkaur84@aol.com>
2020-05-12 00:48:45 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+my_hash = {"Name " => "pari", "Age" => 35, "Language" => "Ruby"}
+
 
   
 end
