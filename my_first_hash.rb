f5c9662a53888e5140346755595f200c4e18517e
PARDEEP KAUR <pkaur84@aol.com>
2020-05-12 00:50:54 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,7 +22,8 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  
+shipping_manifest["oil paintings"]
 end
 
 def adding
